525a7c6ec7b514c541e7b0a91e9e9bb0d9a33462
Guilherme Bublitz <gui.e.bublitz@gmail.com>
2022-01-07 14:56:19 -0200

    this.getTr(user, tr);

--- client-server/public/classes/HttpRequest.js
+++ client-server/public/classes/HttpRequest.js
@@ -53,7 +53,9 @@ class HttpRequest {
 
             };
             
-            ajax.send();
+            ajax.setRequestHeader('Content-Type', 'application/json');
+
+            ajax.send(JSON.stringify(params));
         });
 
         
